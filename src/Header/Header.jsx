ab0bd948da284f390b3f60318a8da8a106271452
Albert Stive Ospina Avendaño <aospina@adgasd32.int>
2023-09-05 10:43:56 -0500

    manga

---
+++ src/Header/Header.jsx
@@ -0,0 +1,20 @@
+
+import './Header.css'
+
+const Header = () =>{
+   return (
+       <header>
+        <figure>
+
+        </figure>
+        <nav>
+            <a href=''>link 1</a>
+            <a href=''>link 2</a>
+            <a href=''>link 3</a>
+        </nav>
+     </header>
+     
+
+  )
+}
+export {Header}
