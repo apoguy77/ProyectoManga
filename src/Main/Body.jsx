83f0b4cb30b17399e12bcd99059d75c771d2cf3e
Laura Galeano <apoguy77@hotmail.com>
2023-09-06 07:59:40 -0500

    prueba1

--- src/Main/Body.jsx
+++ src/Main/Body.jsx
@@ -1,15 +1,14 @@
 import Fondo from '../Img/fondo1.jpg'
-import './Body.css'
+import React from 'react';
+import './Body.css';
 
-const Main = () =>{
-      
+const Main = () => {
     return (
-        <> 
-            <div className="main"></div>
-             <h1> Manga ASD</h1>        
-        </>
-    )
-}         
+        <div className="main">
+            <h1>Manga ASD</h1>
+        </div>
+    );
+}
 
+export default Main;
 
-export {Main}
