59747226d40b99200dcf3c4b8ba9475491c22a25
Laura Galeano <apoguy77@hotmail.com>
2023-09-11 10:35:50 -0500

    base de datos conectada

--- src/MainUser/MainUser.jsx
+++ src/MainUser/MainUser.jsx
@@ -2,19 +2,39 @@ import React, { Component } from 'react';
 import './MainUser.css';
 import axios from 'axios';
 
-// Componente Producto reutilizable
-function Producto({ _id, title, descripcion, imageFileName, CantDis, CanttAl, precio }) {
-    const imageUrl = `/api/mangas/images/${imageFileName}`; // Nueva ruta de la imagen en el backend
-    return (
-        <div className="producto" key={_id}>
-            <img src={imageUrl} alt={title} />
-            <h2>{title}</h2>
-            <p>{descripcion}</p>
-            <p>Valor: ${precio}</p>
-            <p>Cantidad disponible: {CantDis}</p>
-            <p>Disponibles para alquilar: {CanttAl}</p>
-        </div>
-    );
+class Producto extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cantidadDisponible: this.props.CantDis,
+        };
+    }
+
+    handleAlquiler = () => {
+        // Simplemente resta 1 a la cantidad disponible cuando se realiza un alquiler
+        if (this.state.cantidadDisponible > 0) {
+            this.setState((prevState) => ({
+                cantidadDisponible: prevState.cantidadDisponible - 1,
+            }));
+        }
+    };
+
+    render() {
+        const { title, descripcion, image, CanttAl, precio } = this.props;
+        const { cantidadDisponible } = this.state;
+        const imageUrl = `/img/Alquiler/${image}`;
+
+        return (
+            <div className="producto">
+                <img src={imageUrl} alt={title} />
+                <h2>{title}</h2>
+                <p>{descripcion}</p>
+                <p>Valor: ${precio}</p>
+                <p>Cantidad disponible: {cantidadDisponible}</p>
+                <button onClick={this.handleAlquiler}>Alquilar</button>
+            </div>
+        );
+    }
 }
 
 class MainUser extends Component {
@@ -26,7 +46,8 @@ class MainUser extends Component {
     }
 
     componentDidMount() {
-        axios.get('/api/mangas')
+        axios
+            .get('/api/mangas')
             .then((response) => {
                 this.setState({ productos: response.data });
             })
@@ -40,14 +61,14 @@ class MainUser extends Component {
 
         return (
             <div>
-                <h1>Alquiler de Productos</h1>
+                <h1 className='titulo'>Alquiler de Productos</h1>
                 <div className="productos-container">
                     {productos.map((producto) => (
-                        <Producto {...producto} />
+                        <Producto key={producto._id} {...producto} />
                     ))}
                 </div>
             </div>
-        );    
+        );
     }
 }
 
