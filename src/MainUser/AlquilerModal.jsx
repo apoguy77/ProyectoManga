19a8535bbedeab13f64befa274bc2fe03cc248f4
consuelo <apoguy77@hotmail.com>
2023-09-14 08:53:44 -0500

    arreglo conteo

--- src/MainUser/AlquilerModal.jsx
+++ src/MainUser/AlquilerModal.jsx
@@ -1,51 +1,61 @@
 import React, { Component } from 'react';
-import axios from 'axios'; // Importa axios para hacer la solicitud POST
 import './AlquilerModal.css'; // Importa el archivo CSS
 
 class AlquilerModal extends Component {
-    // Función para enviar la solicitud POST al servidor
-    handleAlquilar = async () => {
-        const { mangaNombre, fechaAlquiler, fechaEntrega, estado } = this.props; // Asegúrate de que estos valores estén disponibles en props
-
-        try {
-            const response = await axios.post('/rent', {
-                mangaNombre,
-                fechaAlquiler,
-                fechaEntrega,
-                estado,
-            });
-
-            // Maneja la respuesta según tus necesidades
-            if (response.data.message) {
-                // El alquiler se registró con éxito, puedes mostrar un mensaje de éxito o realizar alguna acción adicional
-                console.log('Alquiler registrado con éxito');
-            } else {
-                // Maneja los posibles errores aquí
-                console.error('Error al registrar el alquiler:', response.data.error);
-            }
-        } catch (error) {
-            // Maneja los errores de la solicitud aquí
-            console.error('Error al enviar la solicitud POST:', error);
-        }
-    }
-
     render() {
-        const { modalVisible, toggleModal, isLoggedIn } = this.props;
+        const { modalVisible, confirmAlquiler, toggleModal, isLoggedIn } = this.props;
+
+        const modalStyle = {
+            display: modalVisible ? 'flex' : 'none',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 1,
+        };
+
+        const modalContentStyle = {
+            backgroundColor: '#fff',
+            padding: '20px',
+            borderRadius: '8px',
+            textAlign: 'center',
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+        };
+
+        const buttonStyle = {
+            backgroundColor: '#007bff',
+            color: '#fff',
+            padding: '10px 20px',
+            margin: '10px',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer',
+            transition: 'background-color 0.3s',
+        };
+
+        const cancelButtonStyle = {
+            backgroundColor: '#ccc',
+            color: '#333',
+        };
 
         return (
-            <div className={modalVisible ? "modal" : "modal-hidden"}>
-                <div className="modal-content">
+            <div style={modalStyle}>
+                <div style={modalContentStyle} className="modal-content">
                     <h2>Confirmar Alquiler</h2>
                     <p>Fecha de Alquiler: {new Date().toLocaleDateString()}</p>
                     <p>Fecha de Entrega: {new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000).toLocaleDateString()}</p>
                     {isLoggedIn ? (
-                        <button onClick={this.handleAlquilar}>Alquilar</button>
+                        <button style={buttonStyle} onClick={confirmAlquiler}>Confirmar Alquiler</button>
                     ) : (
-                        <p>Por favor, inicie sesión para confirmar el alquiler.</p>
+                        <p></p>
                     )}
                     
-                     <button onClick={this.handleAlquilar}>Confirmar Alquiler</button>
-                    <button onClick={toggleModal}>Cancelar</button>
+                    <button style={buttonStyle} onClick={confirmAlquiler}>Alquilar</button>
+                    <button style={{ ...buttonStyle, ...cancelButtonStyle }} onClick={toggleModal}>Cancelar</button>
                 </div>
             </div>
         );
